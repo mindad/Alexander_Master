d5da0113f52c51c51c65ebe6291f1ec869d7e534
BG <bgrass7@gmail.com>
2020-04-04 21:57:49 +0300

    Waste - create process

---
+++ Alexander/Controllers/Waste_LogicController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+//using System.IO;
+
+
+namespace Alexander.Controllers
+{
+    public class Waste_LogicController : ApiController
+    {
+
+        public string Get()
+        {
+            try
+            {
+                // replace with python PATH and send the relevant script in it
+                ProcessStartInfo start = new ProcessStartInfo();
+                start.FileName = @"C:\Windows\System32\cmd.exe";//
+                start.Arguments = @"/C mkdir C:\Users\Owner\Desktop\asd";// /c is needed !!!
+                start.UseShellExecute = false;
+                start.RedirectStandardOutput = true;
+                start.CreateNoWindow = false;
+                start.WindowStyle = ProcessWindowStyle.Normal;
+                using (Process process = Process.Start(start))
+                {
+                    //process.Kill(); // might be needed for killing the py file
+                    process.WaitForExit();
+                }
+
+                return "Made asd Dir";
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+            
+        }
+        
+
+    }
+}
--- Alexander/Models/Waste.cs
+++ Alexander/Models/Waste.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Diagnostics;
+	
+
 
 namespace Alexander.Models
 {
@@ -22,5 +25,8 @@ namespace Alexander.Models
         public float Beer_req_litter { get => beer_req_litter; set => beer_req_litter = value; }
         public int Filling_Hose { get => filling_Hose; set => filling_Hose = value; }
         public int Tank_Leftover { get => tank_Leftover; set => tank_Leftover = value; }
+
+
+
     }
 }
