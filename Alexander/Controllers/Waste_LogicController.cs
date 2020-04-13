2a2d8b965b90fcb8db2d4c63d8691705d85d5e4c
BG <bgrass7@gmail.com>
2020-04-13 18:58:15 +0300

    just some shit

--- Alexander/Controllers/Waste_LogicController.cs
+++ Alexander/Controllers/Waste_LogicController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 using System.Diagnostics;
 using System.Text;
 using System.Threading;
+using System.IO;
 //using System.IO;
 
 
@@ -19,21 +20,32 @@ namespace Alexander.Controllers
         {
             try
             {
+                string result;
                 // replace with python PATH and send the relevant script in it
                 ProcessStartInfo start = new ProcessStartInfo();
-                start.FileName = @"C:\Windows\System32\cmd.exe";//
-                start.Arguments = @"/C mkdir C:\Users\Owner\Desktop\asd";// /c is needed !!!
+                start.FileName = @"C:\Users\Owner\AppData\Local\Programs\Python\Python37-32\python.exe";//
+                start.Arguments = @"C:\Users\Owner\Desktop\waste.py";// 
                 start.UseShellExecute = false;
                 start.RedirectStandardOutput = true;
                 start.CreateNoWindow = false;
                 start.WindowStyle = ProcessWindowStyle.Normal;
+
+                start.RedirectStandardError = true; // process.StandardError
+
                 using (Process process = Process.Start(start))
                 {
                     //process.Kill(); // might be needed for killing the py file
+                    using (StreamReader reader = process.StandardOutput)
+                    {
+                        string stderr = process.StandardError.ReadToEnd(); // Here are the exceptions from our Python script
+                        result = reader.ReadToEnd(); // Here is the result of StdOut(for example: print "test")
+                        
+                    }
+
                     process.WaitForExit();
                 }
 
-                return "Made asd Dir";
+                return result;
             }
             catch (Exception ex)
             {
