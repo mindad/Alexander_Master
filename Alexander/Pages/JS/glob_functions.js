11f142fb9d2a5c92b1aabc40dcc9de22cc4f1c3e
BG <bgrass7@gmail.com>
2020-05-16 14:25:50 +0300

    back button validation - fix

--- Alexander/Pages/JS/glob_functions.js
+++ Alexander/Pages/JS/glob_functions.js
@@ -67,12 +67,19 @@ function checkExpiration_Global() { //check if past expiration date
 function Check_User(user_to_check) {
 
     $(window).on('beforeunload', function () { // Clear localStorage
-        Clear_LocalStorage();
+        
+        if (performance.navigation.type in [0,1]) {
+
+        }
+        else {
+            Clear_LocalStorage();
+        }
     });
     $("a").on('mousedown', function () { // Unclear localStorage in case of moving between pages
         $(window).unbind();
     });
-    $(window).on('submit', function () { $(window).unbind();})
+    $(window).on('submit', function () { $(window).unbind(); })
+
 
     try {
         user_name = localStorage.getItem('user');
@@ -133,20 +140,20 @@ function Create_Navbar() {
     try {
         user_name = localStorage.getItem('user');
     } catch (e) {
-        //window.open('Login.html', '_self');
+        window.open('Login.html', '_self');
     }
     
     if (user_name == 'Manager') {
         $('#which_navbar').html(MANAGER_NAVBAR);
     }
     else {
-        $('#BREWMIESTER_NAVBAR').html(MANAGER_NAVBAR);
+        $('#which_navbar').html(BREWMIESTER_NAVBAR);
     }
     unbind_a_tag();
 }
 
 MANAGER_NAVBAR = `<nav class="navbar navbar-expand-sm navbar-light border-bottom" style="background-color: #f7f6f6;">
-            <a class="navbar-brand" href="Manager\Manager-dashboard.html">Home</a>
+            <a class="navbar-brand" href="Manager/Manager-dashboard.html">Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -182,7 +189,7 @@ MANAGER_NAVBAR = `<nav class="navbar navbar-expand-sm navbar-light border-bottom
         </nav>`;
 
 BREWMIESTER_NAVBAR = `<nav class="navbar navbar-expand-sm navbar-light border-bottom" style="background-color: #f7f6f6;">
-            <a class="navbar-brand" href="Brewmiester-dashboard.html">Home</a>
+            <a class="navbar-brand" href="Brewmiester/Brewmiester-dashboard.html">Home</a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
