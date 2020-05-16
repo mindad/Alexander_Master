6dca0bcc01795be135071efc1386ebae110d6e59
BG <bgrass7@gmail.com>
2020-05-16 12:04:23 +0300

    waste user authentication - fix

--- Alexander/Pages/JS/glob_functions.js
+++ Alexander/Pages/JS/glob_functions.js
@@ -86,7 +86,7 @@ function Check_User(user_to_check) {
     }
 }
 
-function Check_User_Global(user_to_check) {
+function Check_User_Global() {
 
     $(window).on('beforeunload', function () { // Clear localStorage
         Clear_LocalStorage();
@@ -100,11 +100,11 @@ function Check_User_Global(user_to_check) {
         user_name = localStorage.getItem('user');
 
     } catch (e) {
-        window.open('../Login.html', '_self');
+        window.open('Login.html', '_self');
     }
 
-    if (user_to_check != "Brewmiester" || user_name != "Manager") {
-        window.open('../Login.html', '_self');
+    if (user_name != "Brewmiester" && user_name != "Manager") {
+        window.open('Login.html', '_self');
     }
 }
 
@@ -127,3 +127,89 @@ function unbind_a_tag() {
         $(window).unbind();
     });
 }
+
+function Create_Navbar() {
+
+    try {
+        user_name = localStorage.getItem('user');
+    } catch (e) {
+        //window.open('Login.html', '_self');
+    }
+    
+    if (user_name == 'Manager') {
+        $('#which_navbar').html(MANAGER_NAVBAR);
+    }
+    else {
+        $('#BREWMIESTER_NAVBAR').html(MANAGER_NAVBAR);
+    }
+    unbind_a_tag();
+}
+
+MANAGER_NAVBAR = `<nav class="navbar navbar-expand-sm navbar-light border-bottom" style="background-color: #f7f6f6;">
+            <a class="navbar-brand" href="Manager\Manager-dashboard.html">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Manager/Manager-prod.html">Production</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Manager/Manager-ProductInventory.html">Inventory</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Manager/Orders.html">Orders</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Manager/Alerts_Manager.html">Alerts</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Manager/Annual_Reports.html">Annual Reports</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Waste_transition.html">Waste</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Login.html" onclick="Clear_LocalStorage()">Exit</a>
+                    </li>
+                </ul>
+            </div>
+            <a class="navbar-brand" href="#">
+                <img src="Images/Logo_alexander.jpeg" height="68" alt="alexander logo">
+            </a>
+        </nav>`;
+
+BREWMIESTER_NAVBAR = `<nav class="navbar navbar-expand-sm navbar-light border-bottom" style="background-color: #f7f6f6;">
+            <a class="navbar-brand" href="Brewmiester-dashboard.html">Home</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+                    aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="Brewmiester/Brewmiester-prod.html">Production</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Brewmiester/Inventory.html">Inventory</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Brewmiester/Recipes.html">Recipes</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Waste_transition.html">Waste</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Brewmiester/Alerts.html">Alerts</a>
+                    </li>
+                    <li class="nav-item active px-2">
+                        <a class="nav-link" href="Login.html" onclick="Clear_LocalStorage()">Exit</a>
+                    </li>
+                </ul>
+            </div>
+            <a class="navbar-brand" href="#">
+                <img src="Images/Logo_alexander.jpeg" height="68" alt="alexander logo">
+            </a>
+        </nav>`;
